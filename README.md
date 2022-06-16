e266f193b757bfa5b4556926c70972fb5ea1535a
shifeng.lin <shifeng.lin@chukou1.com>
2022-06-16 10:00:15 +0800

    add

--- README.md
+++ README.md
@@ -1,44 +1,78 @@
 # APK-Decompilation-Tools
-A combined tool for APK decompilation.
+【原创】一个APK反编译的组合工具, 使用 Python 进行简单的集成, 方便快捷地对 APK 包快速反编译与打包.
 
-一个APK反编译的组合工具, 使用 Python 进行简单的集成, 方便快捷地对 APK 包快速反编译与打包.
+@Author : 艾登Aiden
+@Email : aiden2048@qq.com
 
-一、平台: 
+
+轮子来源：https://github.com/iBotPeaches/Apktool
+@Author : iBotPeaches
+
+更新 ：
+1. 支持处理多个 classes.dex 文件
+2. apktool.jar 更新到 2.6.1 版本
+
+### 一、平台: 
+
     windows 10 下运行
 
-二、依赖:
+### 二、依赖:
+
     python 环境
+
     java运行环境, 并添加 JAVA_HOME 环境变量
-	
 
+### 三、功能介绍:
 
-三、功能介绍:
     1.反编译(查看) 2.反编译(修改) 3.一键打包并签名 0.退出程序: 
-	
-	tools 目录: 存放主要模块, 不可修改的
 
-四、使用详细：
+### 四、注意事项:
 
-	在 input_apk 目录 放入 目标apk, 比如 xxx.apk (必须的)  (理论上支持多个apk同时进行, 目前还在测试中)
-	
-	执行 run.py
-	
-	选择功能:
-	
-		进行 APK 反编译(查看):
-			执行1
-			在 view 目录下生成 一个项目结构 和 一个 *-jar 目录
-			使用主目录的 jd-gui.exe 工具打开 jar 文件 查看源码
+	1.tools 目录: 存放主要模块, 不可修改的
 
-		APK 解包再打包并签名:
-			先执行2
-			再执行3
-			在 write 目录下生成 xxx 项目文件 + xxx-sign 文件夹
-			xxx-sign.apk 就是可安装的apk
+### 五、使用详细：
 
-# @Author : 艾登Aiden
-# @Email : aiden2048@qq.com
-# @Date : 2022-01-09
+* 克隆
+```bash
+git clone 本项目
+```
+
+* 进入主目录
+```bash
+cd APK-Decompilation-Tools
+```
+
+* 在 input_apk 目录 放入 目标apk, 比如 test.apk (必须的)  (理论上支持多个apk同时进行)
+
+* 启动
+```bash
+python run.py
+```
+
+* 选择功能:
+
+	1. 进行 APK 反编译(查看):
+
+		执行 1
+	
+		在 view 目录下生成 一个项目结构 和 一个 *-jar 目录
+
+		使用主目录的 jd-gui.exe 工具打开 jar 文件 查看源码
+
+	2. 进行 反编译(修改):
+
+		执行 2
+
+	3. APK 解包再打包并签名:
+
+		先执行 2
+
+		再执行 3
+
+		在 write 目录下生成 xxx 项目文件 + xxx-sign 文件夹
+
+		xxx-sign.apk 就是可安装的apk
+
 
 
 (python_3.6.9) D:\Code\My-Github-Code\APK-Decompilation-Tools\APK-Decompilation-Tools>D:/miniconda3/envs/python_3.6.9/python.exe d:/Code/My-Github-Code/APK-Decompilation-Tools/APK-Decompilation-Tools/run.py
--- run.py
+++ run.py
@@ -1,181 +1,268 @@
 # -*- coding: utf-8 -*-
-# @Author : 艾登Aiden
-# @Email : aiden2048@qq.com
-# @Date : 2021-11-25
 
-from genericpath import samestat
 import os
 import time
 import zipfile
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# """ d:\Code\Spider_Armies\APP """
 INPUT_APK_DIR = os.path.join(BASE_DIR, "input_apk")
 TOOLS_DIR = os.path.join(BASE_DIR, "tools")
 VIEW_DIR = os.path.join(BASE_DIR, "view")
 WRITE_DIR = os.path.join(BASE_DIR, "write")
 
+
+print(r"""
+    _    ____  _  __      _         _      _____           _     
+   / \  |  _ \| |/ /     / \  _   _| |_ __|_   _|__   ___ | |___ 
+  / _ \ | |_) | ' /     / _ \| | | | __/ _ \| |/ _ \ / _ \| / __|
+ / ___ \|  __/| . \    / ___ \ |_| | || (_) | | (_) | (_) | \__ \
+/_/   \_\_|   |_|\_\  /_/   \_\__,_|\__\___/|_|\___/ \___/|_|___/
+author: aiden2048                                     
+address: https://github.com/aiden2048
+""")
+
+# 检测目录是否完整
 for dir in [BASE_DIR, INPUT_APK_DIR, TOOLS_DIR, VIEW_DIR, WRITE_DIR]:
     if not os.path.exists(dir):
-        raise Exception("没有文件夹:" + dir)
-
-def scan_dir(path):
-    if not os.path.isdir(path):
-        raise Exception("不是有效路径:" + path)
-    dir_list = []
-    for root, dirs, list in os.walk(path): 
-        for i in list:
-            dir = os.path.join(root, i)
-            if i[-4:] in ".apk":
-                pass
-            dir_list.append([i, dir])
-    return dir_list
-
-APK_LIST = scan_dir(os.path.join(BASE_DIR, "input_apk"))
-if APK_LIST == []:
-    print(".apk文件不存在")
-    time.sleep(3)
-    raise Exception(".apk文件不存在")
+        raise Exception("Error, Not find the directory:" + dir)
 
 
-# apk反编译(查看)
-def view_decompile_apk(apk_name):
+# 检测工具目录
+tools_name = "dex-tools-2.1-SNAPSHOT"
+dex2jar_tools = os.path.join(TOOLS_DIR, tools_name)
+if not os.path.exists(dex2jar_tools):
+    raise Exception("Error, Not find the directory or file:" + dex2jar_tools)
 
-    if apk_name[-4:] not in '.apk':
-        raise Exception('Apk文件不存在')
-    old_file_name  = os.path.join(INPUT_APK_DIR, apk_name)
-    new_file_name  = os.path.join(INPUT_APK_DIR, apk_name[:-4] + '.zip')
+
+def read_all_filename(file_dir):
+    """读取当前目录下的所有文件名
+
+    :param file_dir: 目标文件目录
+    :return: filenames 多文件名
+    """
+    filenames = []
+    for name in os.listdir(file_dir):
+        filenames.append(name)
+    return filenames
+
+
+def get_type_file(file_dir, extensions=['.dex']):
+    """只获取指定后缀类型的文件
+
+    :param file_dir: 目标文件目录
+    :param extensions: 后缀类型, defaults to ['.dex']
+    :return: paths: 多文件路径
+    """
+    paths = []
+    for filename in read_all_filename(file_dir):
+        file_path = os.path.join(file_dir, filename)
+        if os.path.splitext(file_path)[1] in extensions:
+            paths.append(file_path)
+    return paths
+
+
+# paths = get_type_file(BASE_DIR)
+# print(paths)
+
+# def scan_dir(path, extensions=['.apk']):
+#     """递归读取当前目录下的所有文件名
+
+#     :param path: _description_
+#     :raises Exception: _description_
+#     :return: _description_
+#     """
+
+#     dir_list = []
+#     for root, dirs, list in os.walk(path):
+#         for i in list:
+#             dir = os.path.join(root, i)
+#             if i[-4:] in extensions:
+#                 dir_list.append([i, dir])
+#     return dir_list
+
+# paths = scan_dir(INPUT_APK_DIR)
+# print(paths)
+# input_apk_path = os.path.join(BASE_DIR, "input_apk")
+# if not os.path.isdir(input_apk_path):
+#     raise Exception("不是有效路径:" + input_apk_path)
+
+# APK_LIST = scan_dir(INPUT_APK_DIR, extensions=['.apk'])
+# print(APK_LIST)
+# if APK_LIST == []:
+#     raise Exception(".apk文件不存在")
+
+
+def zip_process(apk_tail):
+    """apk反编译(查看)
+
+    :param apk_tail: 文件名+后缀，如app.apk 
+    """
+    name, ext = os.path.splitext(apk_tail)
+    dex_dir = os.path.join(VIEW_DIR, name)
+
+    old_file_name = os.path.join(INPUT_APK_DIR, apk_tail)
+    new_file_name = os.path.join(INPUT_APK_DIR, name + '.zip')
+
     # 更改后缀
     os.rename(old_file_name, new_file_name)
-    # print(old_file_name)
-    # print(new_file_name)
-    name_dir = os.path.join(VIEW_DIR, apk_name[:-4])
-    zip_file = zipfile.ZipFile(new_file_name)
-    for file in zip_file.namelist():
-        zip_file.extract(file, name_dir)
-    zip_file.close()
+
+    zip_obj = zipfile.ZipFile(new_file_name)
+    for file in zip_obj.namelist():
+        zip_obj.extract(file, dex_dir)
+    zip_obj.close()
+
     # 恢复后缀
     os.rename(new_file_name, old_file_name)
-    print(f"Classes.dex file: {os.path.join(name_dir, 'classes.dex')}")
-    # dex2jar
-    dex2jar(name_dir, apk_name[:-4])
+    print("dex file generation directory:", dex_dir)
     return
 
 
-def dex2jar(dir, name):
+def dex2jar(dex, apk_name):
+    """dex 转换为 jar
+
+    :param apk_tail: 文件名+后缀，如app.apk 
     """
-    dir: classes.dex 文件目录
-    name: apk名称, 不带.apk后缀
-    """
-    # dex2jar_tools = "dex-tools-2.1"
-    dex2jar_tools = "dex-tools-2.1-SNAPSHOT"
-    
-    dex2jar_tools_path = os.path.join(TOOLS_DIR, dex2jar_tools)
-    classes_dex = "Classes.dex"
-    
-    # classes.dex 路径
-    classes_dex_path = os.path.join(dir, classes_dex)
+    dex_head, dex_tail = os.path.split(dex)
+    dex_name, ext = os.path.splitext(dex_tail)
+    dex_dir = os.path.join(VIEW_DIR, apk_name)
+
+    # 切换工作目录
+    os.chdir(dex2jar_tools)
+
     # jar生成路径
-    jar_path = os.path.join(f'{dir}-jar', f'{name}_{classes_dex}2jar.jar')
+    jar = os.path.join(f'{dex_dir}-jar', f'{apk_name}_{dex_name}.jar')
 
-    os.chdir(dex2jar_tools_path)
-    os.system(f'd2j-dex2jar.bat -f {classes_dex_path} -o {jar_path}')
+    os.system(f'd2j-dex2jar.bat -f {dex} -o {jar}')
 
-    if not os.path.exists(jar_path):
-            raise Exception(f"错误, 生成 jar 失败, apk: {name}")
+    if not os.path.exists(jar):
+        raise Exception(f"Error, failed to generate jar, apk: {apk_name}")
     else:
-        print(f"Successful jar file: {jar_path}")
-    return
+        print(f"Successful, jar file: {jar}")
+        return
 
 
-def write_decompile_apk(filename):
-    name = filename[:-4]
-    decompile_dir  = os.path.join(WRITE_DIR, name)
+def view_decompile_apk(apk):
+    apk_head, apk_tail = os.path.split(apk)
+    # ('d:\Code\My-Github-Code\APK-Decompilation-Tools\APK-Decompilation-Tools\input_apk, app4.apk')
+    print(apk_head, apk_tail)
+    apk_name, ext = os.path.splitext(apk_tail)
+    dex_dir = os.path.join(VIEW_DIR, apk_name)
+    print(apk_name, ext)  # ('app4', '.apk')
+
+    zip_process(apk_tail)
+    dexs = get_type_file(dex_dir, extensions=['.dex'])
+    for dex in dexs:
+        # 转换
+        dex2jar(dex, apk_name)
+
+
+def write_decompile_apk(apk):
+    # name = filename[:-4]
+    apk_head, apk_tail = os.path.split(apk)
+    apk_name, ext = os.path.splitext(apk_tail)
+
+    decompile_dir = os.path.join(WRITE_DIR, apk_name)
     # 没有目录就新建
     os.path.exists(decompile_dir) or os.mkdir(decompile_dir)
 
     apktool_jar_path = os.path.join(TOOLS_DIR, "apktool.jar")
     os.chdir(TOOLS_DIR)
-    os.system(f'java -jar {apktool_jar_path} d -f {os.path.join(INPUT_APK_DIR, filename)} -o {decompile_dir}')
+    os.system(f'java -jar {apktool_jar_path} d -f {apk} -o {decompile_dir}')
 
     samli_dir = os.path.join(decompile_dir, 'smali')
     if not os.path.exists(samli_dir):
-            raise Exception(f"错误, 生成 smali 失败, apk: {filename}")
+        raise Exception(f"Error, failed to generate smali, apk: {apk_tail}")
     else:
-        print(f"Successful smali dir: {samli_dir}")
-    return
+        print(f"Successful, smali dir: {samli_dir}")
+        return
 
 
-def compile_apk(filename):
-    name = filename[:-4]
-    apktool_jar_path = os.path.join(TOOLS_DIR, "apktool.jar")
-    decompile_dir = os.path.join(WRITE_DIR, name)
+def compile_apk(apk):
+    apk_head, apk_tail = os.path.split(apk)
+    apk_name, ext = os.path.splitext(apk_tail)
 
-    compile_apk_path = os.path.join(decompile_dir, 'dist', f'{name}-new.apk')
-    # xxx文件夹项目打包成xxx-new.apk安装包
-    # java -jar apktool.jar b xxx -o xxx-new.apk
-    
+    apktool_jar = os.path.join(TOOLS_DIR, "apktool.jar")
+    b_dir = os.path.join(WRITE_DIR, apk_name)
+    output_dir = os.path.join(WRITE_DIR, f'{apk_name}-sign')
+    output_apk = os.path.join(b_dir, f'{apk_name}-new.apk')
+
     os.chdir(TOOLS_DIR)
-    os.system(f'java -jar {apktool_jar_path} b -f {decompile_dir} -o {compile_apk_path}')
+    os.system(f'java -jar {apktool_jar} b -f {b_dir} -o {output_apk}')
 
-    if not os.path.exists(compile_apk_path):
-        raise Exception(f"错误, 生成 compile_apk 失败, apk: {filename}")
+    if not os.path.exists(output_apk):
+        raise Exception(f"Error, failed to generate apk, apk: {apk_tail}")
     else:
-        print(f"Successful compile_apk file: {compile_apk_path}")
-    return compile_apk_path
+        print(f"Successful, generate apk : {output_apk}")
+        return output_apk
 
 
-def to_apk_sign(filename):
-    name = filename[:-4]
-    compile_apk_path = os.path.join(WRITE_DIR, name, 'dist', f'{name}-new.apk')
-    compile_apk_sign_dir = os.path.join(WRITE_DIR, f'{name}-sign')
-    compile_apk_sign_path = os.path.join(WRITE_DIR, f'{name}-sign', f'{name}-new-sign.apk')
+def to_apk_sign(apk):
+    apk_head, apk_tail = os.path.split(apk)
+    apk_name, ext = os.path.splitext(apk_tail)
+    # compile_apk_path = os.path.join(WRITE_DIR, apk_name, 'dist', f'{apk_name}-new.apk')
 
+    # 签名apk 保存的文件夹
+    output_dir = os.path.join(WRITE_DIR, f'{apk_name}-sign')
     # 没有目录就新建
-    os.path.exists(compile_apk_sign_dir) or os.mkdir(compile_apk_sign_dir)
-    
+    os.path.exists(output_dir) or os.mkdir(output_dir)
+
+    # apk 的导出路径
+    output_apk = os.path.join(
+        WRITE_DIR, f'{apk_name}-sign', f'{apk_name}-sign.apk')
     os.chdir(TOOLS_DIR)
-    cmd = f'java -jar signapk.jar platform.x509.pem platform.pk8 {compile_apk_path} {compile_apk_sign_path}'
-    os.system(cmd)
-    if not os.path.exists(compile_apk_sign_path):
-        raise Exception(f"错误, 签名失败, apk: {filename}")
+    os.system(
+        f'java -jar signapk.jar platform.x509.pem platform.pk8 {apk} {output_apk}')
+    if not os.path.exists(output_apk):
+        raise Exception(f"Error, failed to sign, apk: {apk}")
     else:
-        print(f"Successful 已签名apk file: {compile_apk_sign_path}")
-    return
+        print(f"Successful, apk sign: {output_apk}")
+        return
 
 
 if __name__ == '__main__':
 
-    num = input("====== 选择模式 ====== \n1.反编译(查看) 2.反编译(修改) 3.一键打包并签名 0.退出程序\n输入: ")
-    if num == '1':
-        print("== 进入反编译-查看模式 ==\n正在生成classes.dex文件...")
-        for filename, _ in APK_LIST:
-            view_decompile_apk(filename)
-        print("Done!")
-        time.sleep(4)
-        
-    elif num == '2':
-        print("== 进入反编译-修改模式 ==\n正在生成smali文件夹...")
-        for filename, _ in APK_LIST:
-            write_decompile_apk(filename)
-        print("Done!")
-        time.sleep(4)
+    while 1:
+        extensions = ['.apk']
+        apks = get_type_file(file_dir=INPUT_APK_DIR, extensions=extensions)
+        print("Find apk: ", apks)
+        if apks == []:
+            raise Exception(
+                f"Error, Not find 'input_apk' directory {extensions}")
+        num = input(
+            "====== 选择模式 ====== \n1.反编译(查看) 2.反编译(修改) 3.一键打包并签名 0.退出程序\n输入: ")
+        if num == '1':
+            print("====== 进入反编译-查看模式 ======")
+            for apk in apks:
+                view_decompile_apk(apk)
+            print("Done!")
+            time.sleep(2)
+            break
 
-    elif num == '3':
-        print("== 进入一键打包并签名 ==\n正在生成apk文件...")
-        for filename, _ in APK_LIST:
-            print("正在打包...")
-            compile_apk(filename)
-            print("正在签名...")
-            to_apk_sign(filename)
-        print("Done!")
+        elif num == '2':
+            print("====== 进入反编译-修改模式 ======")
+            for apk in apks:
+                write_decompile_apk(apk)
+            print("Done!")
+            time.sleep(2)
 
-    elif num == '0':
-        print("手动退出程序...")
-        time.sleep(4)
-        quit()
+        elif num == '3':
+            print("====== 进入一键打包并签名 ======")
+            for apk in apks:
+                print("开始打包...")
+                output_apk = compile_apk(apk)
 
-    else:
-        print("输入错误, 退出程序...")
-        time.sleep(4)
-        quit()
+                print("开始签名...")
+                to_apk_sign(output_apk)
+            print("Done!")
+            time.sleep(2)
+            break
+
+        elif num == '0':
+            print("手动退出程序...")
+            time.sleep(2)
+            quit()
+
+        else:
+            print("Failed to input, exit the program.")
+            time.sleep(2)
+            quit()
